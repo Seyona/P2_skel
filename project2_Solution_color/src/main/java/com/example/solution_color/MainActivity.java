8e43eb3881f2ed236013526953ad34e7122ee485
David <Dgbaker93@gmail.com>
2016-02-23 16:08:16 -0500

    I did a lot of crap....forgot what all I did

--- project2_Solution_color/src/main/java/com/example/solution_color/MainActivity.java
+++ project2_Solution_color/src/main/java/com/example/solution_color/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.solution_color;
 
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Camera;
 import android.graphics.Picture;
 import android.net.Uri;
 import android.os.Bundle;
@@ -22,6 +25,13 @@ public class MainActivity extends AppCompatActivity  {
 
     private ImageView camera, background;
 
+    private final int TAKE_PICTURE = 1;
+
+    private final String PREF_FILE_NAME = "Preferences";
+    private final String DEFAULT_PATH   = "";
+
+    private String Path_To_Picture = DEFAULT_PATH;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,10 +39,17 @@ public class MainActivity extends AppCompatActivity  {
 
         camera = (ImageView) findViewById(R.id.camera);
         background = (ImageView) findViewById(R.id.background);
+        getPref(); // get preferences if there are any
 
+        if (!Path_To_Picture.equals(DEFAULT_PATH)) { //if the current path is not ""
+            changeBackgroundImage(Camera_Helpers.loadAndScaleImage(Path_To_Picture,
+                    background.getWidth(), background.getHeight()));
+        }
 
         setContentView(R.layout.activity_main);
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -69,7 +86,9 @@ public class MainActivity extends AppCompatActivity  {
 
                 break;
             case R.id.restore:
+                removeSavedPhoto();
                 background.setImageResource(R.drawable.gutters);
+                Path_To_Picture = "";
                 changeBackgroundImage(null);
 
 
@@ -86,19 +105,59 @@ public class MainActivity extends AppCompatActivity  {
         Uri  output  = Uri.fromFile(outFile);
         intent.putExtra(MediaStore.EXTRA_OUTPUT,output);
 
-        startActivityForResult(intent, 1);
+        startActivityForResult(intent, TAKE_PICTURE);
+    }
 
-
+    /**
+     * Checks for if the path is not the default path
+     * if it is not the default path then the photo is removed
+     */
+    private void removeSavedPhoto() {
+        if (!Path_To_Picture.equals(DEFAULT_PATH)) {
+            Camera_Helpers.delSavedImage(Path_To_Picture);
+            Path_To_Picture = DEFAULT_PATH;
+        }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == 1) {
-            if (resultCode == RESULT_OK) {
+           switch (resultCode) {
+               case RESULT_OK:
+                   removeSavedPhoto();
+                   Path_To_Picture = data.getData().getPath();
+                   //File picture = new File(data.getData().getPath());
+                   int targetW  = background.getWidth();
+                   int targetH  = background.getHeight();
 
-            } else if (resultCode == RESULT_CANCELED) {
+                   // get dimensions of bitmap
+                   BitmapFactory.Options bmOps = new BitmapFactory.Options();
+                   bmOps.inJustDecodeBounds = true;
+                   BitmapFactory.decodeFile(Path_To_Picture);
+                   int photoW = bmOps.outWidth;
+                   int photoH = bmOps.outHeight;
 
-            }
+                   // Determine how much to scale down the image
+                   int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+
+                   // Decode the image file into a Bitmap sized to fill the View
+                   bmOps.inJustDecodeBounds = false;
+                   bmOps.inSampleSize = scaleFactor;
+                   //bmOps.inPurgeable = true;
+
+                   Bitmap bitmap = BitmapFactory.decodeFile(Path_To_Picture, bmOps);
+                   background.setImageBitmap(bitmap);
+                   Camera_Helpers.saveProcessedImage(bitmap, Path_To_Picture);
+                   savePref();
+                   break;
+
+               case RESULT_CANCELED:
+                   // do nothing?
+                   break;
+
+               default:
+                   break;
+           }
         }
     }
 
@@ -106,6 +165,22 @@ public class MainActivity extends AppCompatActivity  {
         if (photo != null) background.setImageBitmap(photo);
         background.setScaleType(ImageView.ScaleType.FIT_CENTER);
         background.setScaleType(ImageView.ScaleType.FIT_XY);
+    }
+
+
+    public void getPref() {
+        SharedPreferences settings = getSharedPreferences(PREF_FILE_NAME, MODE_PRIVATE);
+
+        Path_To_Picture = settings.getString("Current Picture Path", DEFAULT_PATH);
+    }
+
+    public void savePref() {
+        SharedPreferences settings = getSharedPreferences(PREF_FILE_NAME, MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = settings.edit();
+
+        editor.putString("Current Picture Path", Path_To_Picture);
+        editor.commit();
     }
 }
 
